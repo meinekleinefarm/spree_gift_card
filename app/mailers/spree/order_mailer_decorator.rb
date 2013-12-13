0791dfc7ee78c1e39a1edf7a7b7242c2bd12ae86
Christoph Bünte <info@christophbuente.de>
2013-12-13 13:34:15 +0100

    Add attachment with nice pdf gift card.

--- app/mailers/spree/order_mailer_decorator.rb
+++ app/mailers/spree/order_mailer_decorator.rb
@@ -2,8 +2,9 @@ Spree::OrderMailer.class_eval do
   def gift_card_email(card, order)
     @gift_card = card
     @order = order
-    subject = "#{Spree::Config[:site_name]} Gift Card"
+    subject = "#{Spree::Config[:site_name]} #{t('order_mailer.gift_card_email.subject')}"
     @gift_card.update_attribute(:sent_at, Time.now)
-    mail(:to => card.email, :subject => subject)
+    attachments['Gutschwein.pdf'] = File.read(@gift_card.attachment)
+    mail(:to => @order.email, :subject => subject)
   end
 end
--- app/models/spree/gift_card.rb
+++ app/models/spree/gift_card.rb
@@ -1,7 +1,11 @@
+# coding: utf-8
 require 'spree/core/validators/email'
+require 'prawn'
 
 module Spree
   class GiftCard < ActiveRecord::Base
+
+    include ActionView::Helpers::NumberHelper
 
     UNACTIVATABLE_ORDER_STATES = ["complete", "awaiting_return", "returned"]
 
@@ -14,8 +18,6 @@ module Spree
 
     validates :code,               presence: true, uniqueness: true
     validates :current_value,      presence: true
-    validates :email, email: true, presence: true
-    validates :name,               presence: true
     validates :original_value,     presence: true
 
     before_validation :generate_code, on: :create
@@ -57,11 +59,75 @@ module Spree
       !UNACTIVATABLE_ORDER_STATES.include?(order.state)
     end
 
+    def attachment
+      tmp_file = Tempfile.new(['Gutschwein', '.pdf'])
+      Prawn::Document.generate(tmp_file,
+                                :page_size => 'A4',
+                                :page_layout => :landscape,
+                                :margin => 0,
+                                :info => {
+                                  :Title => "Gutschein Code #{code}",
+                                  :Author => "MeinekleineFarm.org UG (haftungsbeschränkt)",
+                                  :Subject => "Ihr Gutschein von MeinekleineFarm.org",
+                                  :Keywords => "Gutschein, MeineKleineFarm.org, PDF",
+                                  :Creator => "Ruby #{RUBY_VERSION}",
+                                  :CreationDate => Time.now
+                                }
+                              ) do |pdf|
+        pdf.font_families.update("Yanone Kaffeesatz" => {
+          :thin => Rails.root.join('app', 'assets', 'fonts', 'YanoneKaffeesatz-Thin.ttf').to_s,
+          :light => Rails.root.join('app', 'assets', 'fonts', 'YanoneKaffeesatz-Light.ttf').to_s,
+          :normal => Rails.root.join('app', 'assets', 'fonts', 'YanoneKaffeesatz-Regular.ttf').to_s,
+          :bold => Rails.root.join('app', 'assets', 'fonts', 'YanoneKaffeesatz-Bold.ttf').to_s
+        })
+
+        pdf.fill_color '362213'
+        #pdf.fill_color 'ffffff'
+        pdf.font "Yanone Kaffeesatz", :size => 22, :style => :normal
+        pdf.font_size 22
+
+#        raise Prawn::Document::PageGeometry::SIZES["A5"].inspect
+        pdf.image Rails.root.join('app', 'assets', 'images', 'flyer_background.jpg'),
+                :at  => [Prawn::Document::PageGeometry::SIZES["A5"][0], Prawn::Document::PageGeometry::SIZES["A5"][1]],
+                :fit => Prawn::Document::PageGeometry::SIZES["A5"]
+
+        pdf.bounding_box [Prawn::Document::PageGeometry::SIZES["A5"][0] + 42, 183], :width => 110, :height => 91 do
+#          pdf.stroke_color 'FF0000'
+#          pdf.stroke_bounds
+          pdf.font "Yanone Kaffeesatz", :size => 136, :style => :bold do
+            pdf.text number_to_currency(15, :precision => 0 ),
+              :rotate => 8,
+              :rotate_around => :center,
+              :overflow => :shrink_to_fit,
+              :character_spacing => -7.0,
+              :kerning => false,
+              :align => :center,
+              :valign => :center
+          end
+        end
+
+        pdf.bounding_box [Prawn::Document::PageGeometry::SIZES["A5"][0] + 170, 174], :width => 200, :height => 56 do
+#          pdf.stroke_color 'FF0000'
+#          pdf.stroke_bounds
+          pdf.font "Helvetica", :size => 24, :style => :bold do
+            pdf.text "Code: #{code}",
+              :rotate => 5,
+              :rotate_around => :center,
+              :overflow => :shrink_to_fit,
+              :kerning => true,
+              :align => :center,
+              :valign => :center
+          end
+        end
+
+      end
+    end
+
     private
 
     def generate_code
       until self.code.present? && self.class.where(code: self.code).count == 0
-        self.code = Digest::SHA1.hexdigest([Time.now, rand].join)
+        self.code = Digest::SHA1.hexdigest([Time.now, rand].join)[0...8]
       end
     end
 
@@ -73,6 +139,7 @@ module Spree
       self.current_value  = self.variant.try(:price)
       self.original_value = self.variant.try(:price)
     end
+
 
   end
 end
